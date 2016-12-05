7d5d81a783b6cef7a63b022daed5f741f1a52dff
Austin Peterson <austin@akpwebdesign.com>
2016-12-05 16:19:39 -0600

    Update github-listener.
    This commit changes github-listener to use the new TextDecorator API,
    as well as consistently using the sendAlert function rather than
    the inconsistency that was here before. Also add missing libraries to
    package.json and update git-repo-info dependency.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 'use strict';
-const c = require('irc-colors');
 const getRepoInfo = require('git-repo-info');
 const GitHubHook = require('githubhook');
 const glob = require('glob');
@@ -28,35 +27,38 @@ var defaultConfig = {
   enabled: true
 };
 
-class GitHubListener extends global.AKP48.pluginTypes.BackgroundTask {
-  constructor(AKP48, config) {
-    super('GitHubListener', AKP48, config || defaultConfig);
-    if(!config) {
-      global.logger.info(`${this._pluginName}: No config specified. Saving defaults.`);
+class GitHubListener extends global.AKP48.pluginTypes.Generic {
+  constructor(AKP48) {
+    super(AKP48, 'GitHubListener');
+  }
 
-      AKP48.saveConfig(this._config, 'github-listener');
+  load() {
+    if(!this.config) {
+      global.logger.info(`${this.name}: No config specified. Saving defaults.`);
+
+      AKP48.saveConfig(this.config, 'github-listener');
     }
 
     this._isRepo = (getRepoInfo._findRepo('.') !== null);
 
-    if(this._config.enabled) {
+    if(this.config.enabled) {
       this._listener = new GitHubHook({
-        path: this._config.path,
-        port: this._config.port,
-        secret: this._config.secret,
+        path: this.config.path,
+        port: this.config.port,
+        secret: this.config.secret,
         logger: { //define a logger object, so the module doesn't just use console directly.
           log: function(msg){
-            global.logger.silly(`${self._pluginName}|GitHubHook: ${msg}`);
+            global.logger.silly(`${self.name}|GitHubHook: ${msg}`);
           },
           error: function(msg){
-            global.logger.error(`${self._pluginName}|GitHubHook: ${msg}`);
+            global.logger.error(`${self.name}|GitHubHook: ${msg}`);
           }
         }
       });
 
-      global.logger.info(`${this._pluginName}: Listening for Webhooks from GitHub.`);
-      global.logger.debug(`${this._pluginName}: Listening at ${this._config.path} on ${this._config.port}.`);
-      global.logger.silly(`${this._pluginName}: Listening for repo ${this._config.repository}, branch ${this._config.branch}.`);
+      global.logger.info(`${this.name}: Listening for Webhooks from GitHub.`);
+      global.logger.debug(`${this.name}: Listening at ${this.config.path} on ${this.config.port}.`);
+      global.logger.silly(`${this.name}: Listening for repo ${this.config.repository}, branch ${this.config.branch}.`);
 
       this._listener.listen();
 
@@ -65,7 +67,7 @@ class GitHubListener extends global.AKP48.pluginTypes.BackgroundTask {
         if(data.deleted) {
           return;
         }
-        global.logger.silly(`${self._pluginName}: Received Webhook: ref => ${ref}.`);
+        global.logger.silly(`${self.name}: Received Webhook: ref => ${ref}.`);
 
         var branch = ref.substring(ref.indexOf('/', 5) + 1);
 
@@ -73,28 +75,32 @@ class GitHubListener extends global.AKP48.pluginTypes.BackgroundTask {
         var commits = `${data.commits.length} commit`.pluralize(data.commits.length);
         var url = data.compare;
 
-        // TODO: confusing code? Template literal inside a template literal. Template-ception.
-        var msg = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} ${commits} ${data.forced && !data.created ? 'force ' : ''}pushed to ${data.created ? 'new ' : ''}`;
-        msg += `${data.ref.startsWith('refs/tags/') ? 'tag' : 'branch'} ${c.bold(branch)} by ${data.pusher.name}. `;
-        msg += `(${url})`;
+        var msg = [];
+        msg.push({style: 'pink', text: '[Github] '});
+        msg.push({style: 'green', text: `[${repo}] `});
+        msg.push(`${commits} ${data.forced && !data.created ? 'force ' : ''}pushed to ${data.created ? 'new ' : ''}`);
+        msg.push(`${data.ref.startsWith('refs/tags/') ? 'tag' : 'branch'} `);
+        msg.push({style: 'bold', text: `${branch} `});
+        msg.push(`by ${data.pusher.name}. `);
+        msg.push(`(${url})`);
 
         for (var i = 0; i < data.commits.length && i < 3; i++) {
-            var _c = data.commits[data.commits.length - 1 - i];
-            var _m = _c.message;
-            var end = _m.indexOf('\n');
-            var commit_msg = c.green(`[${_c.id.substring(0,7)}] `);
-            commit_msg += `${_c.author.username}: ${_m.substring(0, end === -1 ? _m.length : end)}`;
-            msg += `\n${commit_msg}`;
+          var _c = data.commits[data.commits.length - 1 - i];
+          var _m = _c.message;
+          var end = _m.indexOf('\n');
+          msg.push({style: 'green', text: `[${_c.id.substring(0,7)}] `});
+          msg.push(`${_c.author.username}: ${_m.substring(0, end === -1 ? _m.length : end)}`);
         }
 
         if(self.shouldSendAlert('push')) {
-          global.logger.verbose(`${self._pluginName}: Sending alert.`);
-          self._AKP48.sendMessage(msg, {isAlert: true});
+          global.logger.verbose(`${self.name}: Sending alert.`);
+          AKP48.sendAlert(msg);
         }
 
         if(self.shouldUpdate(branch) && repo === self._config.repository) {
           self.handle(branch, data);
         }
+
       }).on(`pull_request`, function(repo, ref, data) {
         if(!self.shouldSendAlert('pull_request')) { return; }
         if(data.action === 'closed' && data.pull_request.merged) {
@@ -103,9 +109,12 @@ class GitHubListener extends global.AKP48.pluginTypes.BackgroundTask {
         if(data.pull_request.title.length >= 80) {
           data.pull_request.title = data.pull_request.title.substring(0,80) + '...';
         }
-        var out = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} Pull Request ${data.number} ${data.action}. Title: ${data.pull_request.title}`;
 
-        AKP48.emit('alert', out);
+        var out = [];
+        out.push({style: 'pink', text: '[Github] '});
+        out.push({style: 'green', text: `[${repo}] `});
+        out.push(`Pull Request ${data.number} ${data.action}. Title: ${data.pull_request.title}`);
+        AKP48.sendAlert(out);
       }).on(`issues`, function(repo, ref, data) {
         if(!self.shouldSendAlert('issues')) { return; }
         if(data.issue.title.length >= 80) {
@@ -118,43 +127,71 @@ class GitHubListener extends global.AKP48.pluginTypes.BackgroundTask {
         if(data.action === 'labeled' || data.action === 'unlabeled') {
           data.action += ` ${data.label.name}`;
         }
-        var out = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} Issue ${data.issue.number} ${data.action}. Title: ${data.issue.title}`;
-
-        AKP48.emit('alert', out);
+        var out = [];
+        out.push({style: 'pink', text: '[Github] '});
+        out.push({style: 'green', text: `[${repo}] `});
+        out.push(`Issue ${data.issue.number} ${data.action}. Title: ${data.issue.title}`);
+        AKP48.sendAlert(out);
       }).on(`issue_comment`, function(repo, ref, data) {
         if(!self.shouldSendAlert('issue_comment')) { return; }
         if(data.comment.body.length >= 80) {
           data.comment.body = data.comment.body.substring(0,80) + '...';
         }
-        var out = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} New comment on issue ${data.issue.number} by ${c.bold(data.comment.user.login)}. ${data.comment.body} (${data.comment.html_url})`;
-
-        AKP48.emit('alert', out);
+        var out = [];
+        out.push({style: 'pink', text: '[Github] '});
+        out.push({style: 'green', text: `[${repo}] `});
+        out.push(`New comment on issue ${data.issue.number} by `);
+        out.push({style: bold, text: `${data.comment.user.login}. `});
+        out.push(`${data.comment.body} (${data.comment.html_url})`);
+        AKP48.sendAlert(out);
       }).on(`gollum`, function(repo, ref, data) {
         if(!self.shouldSendAlert('gollum')) { return; }
         for (var i = 0; i < data.pages.length; i++) {
           var pg = data.pages[i];
-          var out = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} Wiki Page ${c.bold(pg.page_name)} ${pg.action}. (${pg.html_url})`;
-          AKP48.emit('alert', out);
+          var out = [];
+          out.push({style: 'pink', text: '[Github] '});
+          out.push({style: 'green', text: `[${repo}] `});
+          out.push(`Wiki Page`);
+          out.push({style: bold, text: `${pg.page_name} `});
+          out.push(`${pg.action}. (${pg.html_url})`);
+          AKP48.sendAlert(out);
         }
       }).on(`fork`, function(repo, ref, data) {
         if(!self.shouldSendAlert('fork')) { return; }
-        var out = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} New Fork! ${c.bold(data.sender.login)} forked the repo! (${data.forkee.html_url})`;
-
-        AKP48.emit('alert', out);
+        var out = [];
+        out.push({style: 'pink', text: '[Github] '});
+        out.push({style: 'green', text: `[${repo}] `});
+        out.push(`New Fork!`);
+        out.push({style: bold, text: `${data.sender.login} `});
+        out.push(`forked the repo! (${data.forkee.html_url})`);
+        AKP48.sendAlert(out);
       }).on(`watch`, function(repo, ref, data) {
         if(!self.shouldSendAlert('watch')) { return; }
-        var out = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} New Star! ${c.bold(data.sender.login)} starred the repo!`;
-
-        AKP48.emit('alert', out);
+        var out = [];
+        out.push({style: 'pink', text: '[Github] '});
+        out.push({style: 'green', text: `[${repo}] `});
+        out.push(`New Star!`);
+        out.push({style: bold, text: `${data.sender.login} `});
+        out.push(`starred the repo!`);
+        AKP48.sendAlert(out);
       }).on(`repository`, function(repo, ref, data) {
         if(!self.shouldSendAlert('watch')) { return; }
-        var out = `${c.pink('[GitHub]')} ${c.green(`[${data.organization.login} Organization]`)} Repository ${c.bold(data.repository.name)} ${data.action} by ${c.bold(data.sender.login)}. (${data.repository.html_url})`;
-
-        AKP48.emit('alert', out);
+        var out = [];
+        out.push({style: 'pink', text: '[Github] '});
+        out.push({style: 'green', text: `[${data.organization.login} Organization] `});
+        out.push(`Repository `);
+        out.push({style: 'bold', text: `${data.repository.name} `});
+        out.push(`${data.action} by `);
+        out.push({style: 'bold', text: `${data.sender.login}`});
+        out.push(`. (${data.repository.html_url})`);
+        AKP48.sendAlert(out);
       }).on('commit_comment', function (repo, ref, data) {
         if (!self.shouldSendAlert('commit_comment')) { return; }
-        var out = `${c.pink('[GitHub]')} ${c.green(`[${repo}]`)} ${data.comment.user.login} left a comment. ${data.comment.html_url}`;
-        AKP48.emit('alert', out);
+        var out = [];
+        out.push({style: 'pink', text: '[Github] '});
+        out.push({style: 'green', text: `[${repo}] `});
+        out.push(`${data.comment.user.login} left a comment. ${data.comment.html_url}`);
+        AKP48.sendAlert(out);
       });
     }
   }
@@ -185,7 +222,7 @@ GitHubListener.prototype.compare = function (original, other) {
 };
 
 GitHubListener.prototype.shouldUpdate = function (branch) {
-  var updateBranch = this._config.branch;
+  var updateBranch = this.config.branch;
   if (Array.isArray(updateBranch)) { // We update only if it is listed
       for (var x = 0; x < updateBranch.length; x++) {
           var _branch = updateBranch[x];
@@ -201,21 +238,21 @@ GitHubListener.prototype.shouldUpdate = function (branch) {
 
 GitHubListener.prototype.handle = function (branch, data) {
   var self = this;
-  global.logger.info(`${this._pluginName}: Handling Webhook for branch ${branch}.`);
+  global.logger.info(`${this.name}: Handling Webhook for branch ${branch}.`);
 
   if (!shell.which('git') || !this._isRepo) {
-    global.logger.debug(`${this._pluginName}: Not a git repo; stopping update.`);
+    global.logger.debug(`${this.name}: Not a git repo; stopping update.`);
     return;
   }
 
   var changing_branch = branch !== this.getBranch();
-  var update = this._config.autoUpdate && (data.commits.length !== 0 || changing_branch);
+  var update = this.config.autoUpdate && (data.commits.length !== 0 || changing_branch);
 
-  global.logger.silly(`${this._pluginName}: Is changing branch? ${changing_branch}.`);
-  global.logger.silly(`${this._pluginName}: Is updating? ${update}.`);
+  global.logger.silly(`${this.name}: Is changing branch? ${changing_branch}.`);
+  global.logger.silly(`${this.name}: Is updating? ${update}.`);
 
   if (!update) {
-    global.logger.debug(`${this._pluginName}: Nothing to update; stopping update.`);
+    global.logger.debug(`${this.name}: Nothing to update; stopping update.`);
     return;
   }
 
@@ -250,7 +287,7 @@ GitHubListener.prototype.handle = function (branch, data) {
     }
   }
 
-  global.logger.debug(`${this._pluginName}: Updating to branch "${branch}".`);
+  global.logger.debug(`${this.name}: Updating to branch "${branch}".`);
 
   // Fetch, Checkout
   if (!this.checkout(branch)) {
@@ -258,14 +295,14 @@ GitHubListener.prototype.handle = function (branch, data) {
   }
 
   if(npm) {
-    global.logger.debug(`${this._pluginName}: Executing npm install.`);
+    global.logger.debug(`${this.name}: Executing npm install.`);
     shell.cd(require('app-root-path').path);
     shell.exec('npm install');
   }
 
   var pluginPath = path.resolve(require('app-root-path').path, 'plugins/*/plugin.json');
   glob(pluginPath, function(err, files) {
-    if(err) {global.logger.error(`${this._pluginName}: Glob error: "${err}".`);return;}
+    if(err) {global.logger.error(`${this.name}: Glob error: "${err}".`);return;}
 
     new Promise(function(resolve) {
       //two separate loops because shell is doing something weird if I do it all as one loop.
@@ -282,7 +319,7 @@ GitHubListener.prototype.handle = function (branch, data) {
 
         proms.push(new Promise(function(resolve){ // jshint ignore:line
           if(npm) {
-            global.logger.verbose(`${self._pluginName}: Executing npm install for ${files[j]}.`);
+            global.logger.verbose(`${self.name}: Executing npm install for ${files[j]}.`);
             shell.exec('npm install', function(){
               resolve();
             });
@@ -308,10 +345,10 @@ GitHubListener.prototype.handle = function (branch, data) {
 
 GitHubListener.prototype.fetch = function () {
   if(shell.exec('git fetch').code) {
-    global.logger.error(`${this._pluginName}: Attempted git fetch failed!`);
+    global.logger.error(`${this.name}: Attempted git fetch failed!`);
     return;
   } else {
-    global.logger.verbose(`${this._pluginName}: Fetched latest code from git.`);
+    global.logger.verbose(`${this.name}: Fetched latest code from git.`);
   }
   return true;
 };
@@ -334,30 +371,30 @@ GitHubListener.prototype.checkout = function (branch) {
   }
   if (this.getBranch() !== branch) {
     if (shell.exec(`git checkout -q ${branch}`).code) {
-      global.logger.error(`${this._pluginName}: Attempted git reset failed!`);
+      global.logger.error(`${this.name}: Attempted git reset failed!`);
       return;
     } else {
-      global.logger.verbose(`${this._pluginName}: Successfully checked out branch "${branch}".`);
+      global.logger.verbose(`${this.name}: Successfully checked out branch "${branch}".`);
     }
   }
   if ((this.getBranch() || this.getTag()) && shell.exec(`git reset -q origin/${branch} --hard`).code) {
-    global.logger.error(`${this._pluginName}: Attempted git reset failed!`);
+    global.logger.error(`${this.name}: Attempted git reset failed!`);
     return;
   } else {
-    global.logger.verbose(`${this._pluginName}: Successfully reset to branch "${branch}".`);
+    global.logger.verbose(`${this.name}: Successfully reset to branch "${branch}".`);
   }
   return true;
 };
 
 GitHubListener.prototype.shouldSendAlert = function (hookType) {
-  if(!this._config.events || !this._config.events.hasOwnProperty('commit_comment')) { // legacy config didn't have events object.
-    this._config.events = this._config.events || defaultConfig.events;
-    if (!this._config.events.hasOwnProperty('commit_comment')) { this._config.events.commit_comment = true; }
-    this._AKP48.saveConfig(this._config, 'github-listener');
+  if(!this.config.events || !this.config.events.hasOwnProperty('commit_comment')) { // legacy config didn't have events object.
+    this.config.events = this.config.events || defaultConfig.events;
+    if (!this.config.events.hasOwnProperty('commit_comment')) { this.config.events.commit_comment = true; }
+    this._AKP48.saveConfig(this.config, 'github-listener');
     return true;
   }
 
-  if(this._config.events[hookType]) {return true;} // hookType is enabled in config.
+  if(this.config.events[hookType]) {return true;} // hookType is enabled in config.
   return false;
 };
 
